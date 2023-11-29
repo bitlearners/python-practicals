977e6f4b26ef45deae31619fbc645012aebd3b60
Bitlearners <parateabhishek404@gmail.com>
2023-11-29 22:34:14 +0530

    Create Write a Python Program to Check if a Number is Positive, Negative or 0.py

---
+++ Write a Python Program to Check if a Number is Positive, Negative or 0.py
@@ -0,0 +1,22 @@
+# Developer: Bitlearners
+
+# Input from the user
+number = float(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+    print("The entered number is positive.")
+elif number < 0:
+    print("The entered number is negative.")
+else:
+    print("The entered number is zero.")
+
+
+'''
+The program takes input from the user for a number using float(input("Enter a number: ")).
+It uses an if-elif-else statement to check if the entered number is positive, negative, or zero.
+If the number is greater than 0, it prints "The entered number is positive."
+If the number is less than 0, it prints "The entered number is negative."
+If the number is neither greater nor less than 0 (i.e., it is equal to 0), it prints "The entered number is zero."
+
+'''
