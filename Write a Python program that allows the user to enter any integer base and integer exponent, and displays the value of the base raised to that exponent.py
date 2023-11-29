6d1bed319b8e7e1eaef0c62d9c3cfe93e749288f
Bitlearners <parateabhishek404@gmail.com>
2023-11-29 22:37:43 +0530

    Update Write a Python program that allows the user to enter any integer base and integer exponent, and displays the value of the base raised to that exponent.py

--- Write a Python program that allows the user to enter any integer base and integer exponent, and displays the value of the base raised to that exponent.py
+++ Write a Python program that allows the user to enter any integer base and integer exponent, and displays the value of the base raised to that exponent.py
@@ -1,3 +1,4 @@
+# Developer: Bitlearners
 # Write a Python program that allows the user to enter any integer base and integer exponent, and displays the value of the base raised to that exponent.
 base = int(input("Enter the base (an integer): "))
 exponent = int(input("Enter the exponent (an integer): "))
