63f80ace9db3e94b3b9805d83f795d93c18fc021
Bitlearners <parateabhishek404@gmail.com>
2023-11-29 22:42:15 +0530

    Create Write a Python to Check if a Number is Odd or Even.py

---
+++ Write a Python to Check if a Number is Odd or Even.py
@@ -0,0 +1,27 @@
+#Write a Python Program to Check if a Number is Odd or Even
+
+
+# Developer: Bitlearners
+
+number = int(input("Enter a number: "))
+
+# Check if the number is odd or even
+if number % 2 == 0:
+    print("The entered number is even.")
+else:
+    print("The entered number is odd.")
+
+'''
+
+Explanation:
+
+The program takes input from the user for a number using int(input("Enter a number: ")).
+It uses the modulo operator % to check if the number is divisible by 2.
+If the remainder is 0, it means the number is even, and the program prints "The entered number is even."
+If the remainder is not 0, it means the number is odd, and the program prints "The entered number is odd."
+
+
+
+
+
+'''
